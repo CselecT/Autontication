35ccaf87714cf1c53a2b846ce09d5fd0b272a4ce
smgtkn <smgtknn@gmail.com>
2022-06-29 00:28:39 +0300

    update

--- .workspaces/default_workspace/contracts/CarMessaging.sol
+++ .workspaces/default_workspace/contracts/CarMessaging.sol
@@ -6,15 +6,64 @@ import "./CarRole.sol";
 import "./CentralRole.sol";
 
 contract CarMessaging is CarRole,CentralRole{
+    struct Car{
+        
+        string plate;
+        int posY;
+        int posX;
+    }   
+    event PositionUpdated(address indexed account);
+   
+    address[] listOfAddresses;
     string public message;
 
+    mapping (address => Car) private cars;
+ 
     function hi() public onlyCentral{
         message = "Hi i am central";
     }
+    
+  address[] closeCars;
 
     function registerCar(address requestSender) public onlyCentral{
         addCar(requestSender);
+        bool contains=false;
+        for (uint i =0 ;i< listOfAddresses.length;i++){
+              if (requestSender == listOfAddresses[i]) {
+               contains = true;   
+                break;             
+            }          
+        }
+        if(!contains){
+            listOfAddresses.push(requestSender);
+        }
     }
 
+    function getCloseCars() public onlyCars returns(address[] memory){
+        closeCars=new address[](listOfAddresses.length);
+        int senderX=cars[msg.sender].posX;
+        int senderY=cars[msg.sender].posY;
+        for (uint i =0 ;i< listOfAddresses.length;i++){
+                int proximity= ((cars[listOfAddresses[i]].posY-senderY)**2+(cars[listOfAddresses[i]].posX-senderX)**2);
+           if(msg.sender!=listOfAddresses[i] && proximity<10**2){ 
+                  
+              
+                  closeCars[i]=listOfAddresses[i]; 
+
+                }
+        }
+        return closeCars;
+
+    }
+
+    function updatePosition(int x,int y) public onlyCars {
+       
+        cars[msg.sender].posX=x;
+        cars[msg.sender].posY=y;
+        emit PositionUpdated(msg.sender);
+    }
+
+
+ 
     
 }
